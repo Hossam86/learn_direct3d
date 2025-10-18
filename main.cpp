30b0b0f37d4872f2092f4b15ef89e416bb22001e
Hossam Ali <hossamsamir@outlook.com>
2025-10-18 15:41:01 +0300

    Add initial implementation of Window class

--- main.cpp
+++ main.cpp
@@ -1,45 +1,11 @@
-#include <windows.h>
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	default:
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "Window.h"
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 					 LPSTR lpCMDLine, int nCmdShow)
 {
-	// first configuration class
-	WNDCLASSEX wc = {0};
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.hIconSm = nullptr;
-	const auto pClassName = "hw3butts";
-	wc.lpszClassName = pClassName;
-
-	// register window class
-	RegisterClassEx(&wc);
-
 	// create instance of window class
-	HWND hWnd =
-		CreateWindowExA(0, pClassName, "Happy Hard Window",
-						WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, 200, 200, 640,
-						480, nullptr, nullptr, hInstance, nullptr);
-
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 170, "Donkey Fart Box");
+	Window wnd2(200, 650, "Donkey Fart Box2");
 
 	MSG msg;
 	BOOL gResult;
